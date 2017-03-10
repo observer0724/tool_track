58d26d584be790711031f876b040b87fc535abbd
observer0724 <yxz1128@case.edu>
2017-03-09 21:44:33 -0500

    Add files via upload

--- finder.py
+++ finder.py
@@ -1,14 +1,16 @@
 import cv2 as cv
 import numpy as np
-matrix = np.zeros((13,5,3))
-folder = "/home/yida/Downloads/yida_pictures/9/not_touch/"
-img = cv.imread(folder + "55.png")
-matrix = img[411:425,298:303,:]
-
-matrix_1 = matrix[:,:,0]
-matrix_2 = matrix[:,:,1]
-matrix_3 = matrix[:,:,2]
-
-print np.max(matrix_1),np.min(matrix_1)
-print np.max(matrix_2),np.min(matrix_2)
-print np.max(matrix_3),np.min(matrix_3)
+matrix = np.zeros((21,9,3))
+folder = "/home/user/Desktop/yida/yida_pictures/9/not_touch/"
+img = cv.imread(folder + "68.png")
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+# matrix = hsv[175:295,295:303,:]
+#
+# matrix_1 = matrix[:,:,0]
+# matrix_2 = matrix[:,:,1]
+# matrix_3 = matrix[:,:,2]
+#
+# print np.max(matrix_1),np.min(matrix_1)
+# print np.max(matrix_2),np.min(matrix_2)
+# print np.max(matrix_3),np.min(matrix_3)
+print hsv[299,182,:]
--- select.py
+++ select.py
@@ -3,7 +3,7 @@ import numpy as np
 import glob as gl
 from matplotlib import pyplot as plt
 
-folder = "/home/yida/Downloads/yida_pictures/9/not_touch/"
+folder = "/home/user/Desktop/yida/yida_pictures/11/touch/"
 names = gl.glob(folder + "*.png")
 
 
@@ -14,32 +14,61 @@ def filter(img):
     filted = cv.inRange(img,lower, upper)
     filted_2 = np.zeros(np.shape(img))
     filted_2[:,:,2] = filted
-    return filted,filted_2
+    # return filted,filted_2
     return filted_2
 
 
 def filter_2(img):
-    lower = np.array([130,123,128])
+    lower = np.array([120,123,128])
     upper = np.array([152,186,183])
     filted = cv.inRange(img,lower, upper)
     filted_2 = np.zeros(np.shape(img))
     filted_2[:,:,2] = filted
-    return filted,filted_2
-    # return filted_2
+    # return filted,filted_2
+    return filted_2
 
 
-# for i in range(len(names)):
-#     filename = names[i]
-#     pic_num = int(filename.replace(folder,"").replace(".png",""))
-#     savename = "/home/yida/Desktop/gripper/not_touch/"+str(pic_num)+".png"
-#     img = cv.imread(filename)
-#     # img_g = cv.GaussianBlur( img,(,11), 0, 0, cv.BORDER_DEFAULT )
-#     new_img = img + filter(img)
-#     cv.imwrite(savename,new_img)
-pic_17 = cv.imread(folder+"68.png")
-filted, filted_2 = filter(pic_17)
-plt.imshow(filted)
-# plt.imshow(filted_2+pic_17)
-plt.show()
-# print pic_17[320,355,:]
-# print pic_17[363,418,:]
+
+
+def do_canny(img):
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray2 = gray[5:,:]
+    canny = cv.Canny(gray2,200,250)
+    return canny
+def do_sobel(img):
+    img_g = cv.GaussianBlur( img,(3,3), 0, 0, cv.BORDER_DEFAULT )
+    gray = cv.cvtColor(img_g, cv.COLOR_BGR2GRAY)
+    sobelx = cv.Sobel(gray,cv.CV_16S,1,0,ksize=3)
+    sobely = cv.Sobel(gray,cv.CV_16S,0,1,ksize=3)
+    abs_x = cv.convertScaleAbs(sobelx)
+    abs_y = cv.convertScaleAbs(sobely)
+    grad = cv.addWeighted(abs_x, 0.5, abs_y, 0.5, 0)
+    grad_2 = cv.inRange(grad,np.array([10]),np.array([75]))
+    return grad_2
+for i in range(len(names)):
+    filename = names[i]
+    pic_num = int(filename.replace(folder,"").replace(".png",""))
+    savename = "/home/user/Desktop/yida/gripper/touch/"+str(pic_num)+".png"
+    img = cv.imread(filename)
+    filted = do_canny(img)
+    # deleter = np.zeros(np.shape(img))
+    # deleter[:,:,0] = filted
+    # deleter[:,:,1] = filted
+    # deleter[:,:,2] = filted
+    # del_pic = img-deleter
+    temper = np.zeros(np.shape(img))
+    temper[5:,:,1] = filted
+    new_img = img + temper
+    cv.imwrite(savename,new_img)
+# pic_17 = cv.imread(folder+"22.png")
+#
+# filted = do_sobel(pic_17)
+# deleter = np.zeros(np.shape(pic_17))
+# deleter[:,:,0] = filted
+# deleter[:,:,1] = filted
+# deleter[:,:,2] = filted
+# del_pic = pic_17-deleter
+# new_pic = filter(del_pic)
+# cv.imwrite("/home/user/Desktop/filted.png",new_pic)
+# plt.imshow(filted)
+# plt.show()
